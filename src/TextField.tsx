d53eb9adcf74b9c36928c1394161b7e1aac6f240
dannyirwin <dirwin123@gmail.com>
2021-08-12 15:13:33 -0600

    basic components

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 interface Person {
     firstName: string;
@@ -8,8 +8,13 @@ interface Person {
 interface Props {
     text: string;
     person?: Person;
+    handleChange: () => void;
 }
 
-export const TextField: React.FC<Props> = () => {
-    return <div><input /></div>
+export const TextField: React.FC<Props> = ({handleChange}) => {
+    const [count, setCount] = useState<number | null>(5);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const divRef = useRef<HTMLDivElement>(null);
+
+    return <div ref={divRef}><input ref={inputRef} onChange={handleChange}/></div>
 }
